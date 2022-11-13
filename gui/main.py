a9ade7b8f024ba62d10b2b677cd7afe1e64879a1
Mordekai123 <97954182+Mordekai123@users.noreply.github.com>
2022-11-13 04:19:45 +0300

    update

--- gui/main.py
+++ gui/main.py
@@ -1,61 +1,91 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QPushButton, QLabel, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QPushButton, QLabel, QMessageBox, QCheckBox
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtGui
 from pathlib import Path
 import os
 import sys
 
-
 class UI(QMainWindow):
+
 
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi("main.ui", self)
         self.setWindowIcon(QtGui.QIcon('res/icon.jpg'))
 
+        self.imagelabel = self.findChild(QLabel, "label")
+        self.infolabel = self.findChild(QLabel, "label_2")
+        self.filenamelabel = self.findChild(QLabel, "label_3")
+
         self.current_file = QtGui.QPixmap('res/icon.jpg')
         pixmap = QtGui.QPixmap(self.current_file)
-        self.label.setPixmap(pixmap)
-        self.label.setMinimumSize(1, 1)
+        self.imagelabel.setPixmap(pixmap)
+        self.imagelabel.setMinimumSize(1, 1)
         self.file_list = None
         self.file_counter = None
 
         self.directoryButton = self.findChild(QPushButton, "pushButton")
         self.nextButton2 = self.findChild(QPushButton, "pushButton_2")
         self.prevButton3 = self.findChild(QPushButton, "pushButton_3")
+        self.Button4 = self.findChild(QPushButton, "pushButton_4")
+        self.checkBox = self.findChild(QCheckBox, "checkBox")
 
-        self.directoryButton.clicked.connect(self.open_file)
-        self.nextButton2.clicked.connect(self.next_image)
-        self.prevButton3.clicked.connect(self.previous_image)
+        if self.file_list is None:
+            self.nextButton2.setEnabled(False)
+            self.prevButton3.setEnabled(False)
+            self.Button4.setEnabled(False)
+            self.filenamelabel.setEnabled(False)
 
-    def open_file(self):
+        self.directoryButton.clicked.connect(self.OpenFile)
+        self.nextButton2.clicked.connect(self.NextImage)
+        self.prevButton3.clicked.connect(self.PreviosImage)
+
+    def OpenFile(self):
         folder = QFileDialog.getExistingDirectory()
         self.file_list = [str(file) for file in [*Path(str(folder)).glob('**/*.jpg')]]
         self.image_counter = 0
         self.file_counter = self.file_list[self.image_counter]
+        self.filenamelabel.setText(Path(self.file_counter).name)
         pixmap = QtGui.QPixmap(self.file_counter)
-        self.label.setPixmap(pixmap)
-        print(self.image_counter)
-        print(self.file_list)
+        self.imagelabel.setPixmap(pixmap)
 
-    def next_image(self):
+        if self.checkBox.isChecked():
+            self.infolabel.setText((Path(self.file_counter).name))
+        else:
+            self.infolabel.setText("")
+
+        self.nextButton2.setEnabled(True)
+        self.prevButton3.setEnabled(True)
+        self.Button4.setEnabled(True)
+        self.filenamelabel.setEnabled(True)
+
+    def NextImage(self):
         try:
             self.file_counter = self.file_list[abs(self.image_counter + 1)]
-            print(Path(self.file_counter).name)
+            self.filenamelabel.setText(Path(self.file_counter).name)
             self.image_counter += 1
             pixmap = QtGui.QPixmap(self.file_counter)
-            self.label.setPixmap(pixmap)
-            print()
+            self.imagelabel.setPixmap(pixmap)
+
+            if self.checkBox.isChecked():
+                self.infolabel.setText((Path(self.file_counter).name))
+            else:
+                self.infolabel.setText("")
         except:
             pass
 
-    def previous_image(self):
+    def PreviosImage(self):
         try:
             self.file_counter = self.file_list[abs(self.image_counter - 1)]
-            print(Path(self.file_counter).name)
+            self.filenamelabel.setText(Path(self.file_counter).name)
             self.image_counter -= 1
             pixmap = QtGui.QPixmap(self.file_counter)
-            self.label.setPixmap(pixmap)
+            self.imagelabel.setPixmap(pixmap)
+
+            if self.checkBox.isChecked():
+                self.infolabel.setText((Path(self.file_counter).name))
+            else:
+                self.infolabel.setText("")
         except:
             pass
 
@@ -65,10 +95,8 @@ def main():
     window.show()
     return window
 
-
 if __name__ == "__main__":
     import sys
-
     app = QApplication(sys.argv)
     window = main()
     app.exec_()
